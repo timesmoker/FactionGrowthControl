039b05ce20519e7577b992f6ca6d34ba3faad56d
timesmoker <96424996+timesmoker@users.noreply.github.com>
2025-06-05 13:12:24 +0900

    feat: add cap on total mission count during generation

---
+++ src/MissionCountControl.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace FactionGrowthControl
+{
+  [HarmonyPatch(typeof(FactionSystem), "FactionsTick")]
+  public static class MissionCountControl
+  {
+    
+    private static readonly System.Random Rng = new System.Random();
+
+    public static void Shuffle<T>(IList<T> list)
+    {
+      int n = list.Count;
+      while (n > 1)
+      {
+        n--;
+        int k = Rng.Next(n + 1);
+        (list[n], list[k]) = (list[k], list[n]);
+      }
+    }
+    
+    
+    [HarmonyPatch(typeof(FactionSystem), "FactionsTick")]
+    public static bool Prefix(
+      MissionFactory missionFactory,
+      Missions missions,
+      Stations stations,
+      News news,
+      Factions factions,
+      SpaceTime spaceTime,
+      SpaceTickTimers spaceTickTimers,
+      TravelMetadata travelMetadata,
+      FactionCachedData cache)
+    {
+      
+      double totalHours = (spaceTime.Time - spaceTickTimers.LastFactionsTick).TotalHours;
+      float tickIntervalHours = Data.Global.FactionTickIntervalHours;
+      if (totalHours < tickIntervalHours)
+        return false;
+
+      CacheSystem.CacheStationOwners(cache, stations);
+
+      for (; totalHours >= tickIntervalHours; totalHours -= tickIntervalHours)
+      {
+        spaceTickTimers.LastFactionsTick = spaceTickTimers.LastFactionsTick.AddHours(tickIntervalHours);
+
+        if (missions.Values.Count < 75)
+        {
+          var factionList = factions.Values.ToList();
+          Shuffle(factionList);
+          foreach (var faction in factionList)
+          {
+
+            if (
+              !factions.IsEnabledFaction(faction) ||
+              !FactionSystem.IsMissionChanceApproved(faction) ||
+              FactionSystem.IsMissionsCountReachTechLevelLimit(faction, missions) ||
+              !cache.factionsOwnedStations.TryGetValue(faction.Id, out var stationList) ||
+              stationList.Count == 0)
+              continue;
+
+            FactionSystem.TryCreateMission(faction, missionFactory, missions, stations, news, factions, spaceTime,
+              travelMetadata, cache);
+          }
+        }
+      }
+
+      return false; 
+    }
+  }
+}
